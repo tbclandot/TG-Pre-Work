6aec238794e2acf379d11197998a9442f03da703
TylerBC <tbclandot@gmail.com>
2018-11-18 14:22:47 -0500

    finished learning iterators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/Iterator-Documentation.js
@@ -0,0 +1,15 @@
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+
+
+console.log(words.some(word => {
+  return word.length < 6;
+}));
+
+
+const interestingWords = words.filter((word) => {return word.length > 5});
+
+
+console.log(interestingWords.every((word) => {return word.length > 5}));
+
+// Learned more iterator uses via MDN documentation
